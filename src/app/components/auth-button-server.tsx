8be8e7a5ce32fc408fe7732bcab4f4a48ec1dc10
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-11-27 03:38:22 -0300

    auth0

--- src/app/components/auth-button-server.tsx
+++ src/app/components/auth-button-server.tsx
@@ -3,9 +3,17 @@ import { cookies } from 'next/headers';
 import { AuthButton } from './auth-button-client';
 
 
+
 export async function AuthButtonServer () {
   const supabase = createServerComponentClient({ cookies })
   const { data: { session } } = await supabase.auth.getSession()
 
-  return <AuthButton session={session} />
+
+  return (
+    <section className='grid place-content-center min-h-screen'>
+      <h1 className='text-xl font-bold mb-4'>Iniciar sesion en DevTer</h1>
+  <AuthButton session={session} />
+  </section>
+)
+
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { AuthButtonServer } from "@/app/components/auth-button-server";
+
+export default function Login(){
+  return(
+    <section className="grid place-content-center min-h-screen">
+    <h1>
+      Inicia sesión en DevTer
+    </h1>
+    <AuthButtonServer />   
+    </section>
+  )
+}
